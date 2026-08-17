dc10381ccc6531e098dc7d99de57816e90e70529
luiszambrano232-ai <luiszambrano232@gmail.com>
2026-08-16 23:36:41 -0300

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-import { MenuController } from './controller/menuController.js';
-import { MenuView } from './view/menuView.js';
-const controller = new MenuController();
-const view = new MenuView(controller);
-view.init();
